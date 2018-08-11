3ed0601cafafe1e1e3cc4322391a6eba43e44266
ishita sharma <ishitasharma12699@gmail.com>
2018-08-11 23:10:43 +0530

    Adding FloatingWidgetService and binding up MainActivity : Widget Ready

---
+++ app/src/main/java/com/ishitasharma18/readme/FloatingViewService.java
@@ -0,0 +1,137 @@
+package com.ishitasharma18.readme;
+
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+public class FloatingViewService extends Service implements View.OnClickListener{
+
+    private WindowManager mWindowManager;
+    private View mFloatingView;
+    private View collapsedView;
+    private View expandedView;
+
+    public FloatingViewService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+        //getting the widget layout from xml using layout inflater
+        mFloatingView = LayoutInflater.from(this).inflate(R.layout.floating_widget, null);
+
+        //setting the layout parameters
+        final WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.TYPE_PHONE,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.TRANSLUCENT);
+
+
+        //getting windows services and adding the floating view to it
+        mWindowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        mWindowManager.addView(mFloatingView, params);
+
+
+        //getting the collapsed and expanded view from the floating view
+        collapsedView = mFloatingView.findViewById(R.id.layoutCollapsed);
+        expandedView = mFloatingView.findViewById(R.id.layoutExpanded);
+
+        //adding click listener to close button and expanded view
+        collapsedView.setOnClickListener(this);
+        expandedView.findViewById(R.id.voice_button).setOnClickListener(this);
+        expandedView.findViewById(R.id.cancel_button).setOnClickListener(this);
+        expandedView.findViewById(R.id.flip_back_button).setOnClickListener(this);
+        expandedView.findViewById(R.id.pause_button).setOnClickListener(this);
+
+        //adding an touch listener to make drag movement of the floating widget
+        mFloatingView.findViewById(R.id.relativeLayoutParent).setOnTouchListener(new View.OnTouchListener() {
+            private int initialX;
+            private int initialY;
+            private float initialTouchX;
+            private float initialTouchY;
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        initialX = params.x;
+                        initialY = params.y;
+                        initialTouchX = event.getRawX();
+                        initialTouchY = event.getRawY();
+                        return true;
+
+                    case MotionEvent.ACTION_UP:
+                        //when the drag is ended switching the state of the widget
+                        collapsedView.setVisibility(View.GONE);
+                        expandedView.setVisibility(View.VISIBLE);
+                        return true;
+
+                    case MotionEvent.ACTION_MOVE:
+                        //this code is helping the widget to move around the screen with fingers
+                        params.x = initialX + (int) (event.getRawX() - initialTouchX);
+                        params.y = initialY + (int) (event.getRawY() - initialTouchY);
+                        mWindowManager.updateViewLayout(mFloatingView, params);
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mFloatingView != null)
+            mWindowManager.removeView(mFloatingView);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.layoutCollapsed:
+                //switching views
+                collapsedView.setVisibility(View.GONE);
+                expandedView.setVisibility(View.VISIBLE);
+                break;
+
+            case R.id.cancel_button:
+                //closing the widget
+                stopSelf();
+                break;
+            case R.id.voice_button :
+                startVoice();
+                break;
+            case R.id.pause_button:
+                pauseVoice();
+                break;
+            case R.id.flip_back_button:
+                collapsedView.setVisibility(View.VISIBLE);
+                expandedView.setVisibility(View.GONE);
+                break;
+        }
+    }
+
+    private void pauseVoice() {
+        Toast.makeText(this,"Voice starts",Toast.LENGTH_SHORT).show();
+    }
+
+    private void startVoice() {
+        Toast.makeText(this,"pauses!",Toast.LENGTH_SHORT).show();
+    }
+
+}
